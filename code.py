f177d93a497b0deb5f6c9b713656494b21aec88e
fivawyr <finn.github@gmail.com>
2026-09-19 22:11:01 +0200

    added class

--- code.py
+++ code.py
@@ -3,4 +3,26 @@ from math import *
 
 A = vector(1,2,-1)
 
+data = ["one", "two", "three"]
+for idx, val in enumerate(data): print(f'{idx}:{val}')
+
 box(color=color.red)
+
+class Person:
+    def __init__(self, name, alter):
+        self.name = name
+        self.alter = alter
+
+"""
+class Person {
+public:
+    Person(std::string name, int alter) {
+        this->name = name;
+        this->alter = alter;
+    }
+
+private:
+    std::string name;
+    int alter;
+};
+"""
