f65ae2a923d50b1033284f2139b394e84ac7b0f1
fivawyr <finn.github@gmail.com>
2026-09-20 17:24:06 +0200

    finished project

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 ### Overview 
 - this is a tiny python VPython kinematic 1D simulation 
 ```python
-#for vPython 
-
  python3 -m venv .venv                     
+ pip install VPython 
  source .venv/bin/activate 
 .venv/bin/python -m pip install "setuptools<81"
 ```
+![Screenshot](resources/Screenshot.png)
+> Screenshot from the simulation
 ### Resources
 [Dot Physics](https://www.youtube.com/watch?v=PPIl6cirfhY&list=PLWFlMBumSLSaqgYlK2wD7XSPRQ2p-_Tl4)
--- code.py
+++ code.py
@@ -1,13 +1,32 @@
-from vpython import box, color, vector
-from math import *
+from vpython import *
 
-A = vector(1,2,-1)
+scene = canvas(title = "1D Kinematik Simulation", width = 800, height = 600, background=color.gray(0.1))
 
-data = ["one", "two", "three"]
-for idx, val in enumerate(data): print(f'{idx}:{val}')
+road = box(pos=vec(0, -0.5, 0), size=vec(100, 0.2, 4), color = color.gray(0.5))
 
-box(color=color.red)
+car = box(pos=vec(-40, 0.5, 0), size=vec(4, 1.5, 2), color = color.cyan, make_trail = True, trail_color = color.yellow)
 
+x = car.pos.x 
+v = 2.0
+a = 1.5
+t = 0
+dt = 0.01
+graph_pos = gcurve(color = color.cyan, label = "Position x (m)")
+graph_vel = gcurve(color = color.green, label = "Speed v (m/s)")
+graph_acc = gcurve(color = color.red, label = "Acceleration a (m/s")
+while car.pos.x < 40: 
+    rate(100)
+    v = v + a * dt
+    x = x + v * dt
+    t = t + dt
+    car.pos.x = x 
+    graph_pos.plot(t, x)
+    graph_vel.plot(t, v)
+    graph_acc.plot(t, a)
+    print(f"Reached goald at t = {round(t, 2)} secs")
+
+
+# Workshop Class notes
 class Person:
     def __init__(self, name, alter):
         self.name = name
