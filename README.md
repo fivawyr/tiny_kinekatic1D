d5a15c5e93e4f04624b831f9a9bd103a7a2b29ad
fivawyr <finn.github@gmail.com>
2026-09-22 20:30:31 +0200

    added formula to README

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ### Overview 
 - this is a tiny python VPython kinematic 1D simulation 
+- Formula: 
+$$
+x_2 = x_1 + v_avg\Delta t \rarr  (0m) + (0.45m/s) * (1.5s)
+$$
+- 
 ```python
  python3 -m venv .venv                     
  pip install VPython 
--- code.py
+++ code.py
@@ -10,38 +10,21 @@ x = car.pos.x
 v = 2.0
 a = 1.5
 t = 0
-dt = 0.01
+dt = .01
 graph_pos = gcurve(color = color.cyan, label = "Position x (m)")
 graph_vel = gcurve(color = color.green, label = "Speed v (m/s)")
 graph_acc = gcurve(color = color.red, label = "Acceleration a (m/s")
+
 while car.pos.x < 40: 
     rate(100)
     v = v + a * dt
     x = x + v * dt
-    t = t + dt
+    print(f"t = {t}")
+    t += t
+
     car.pos.x = x 
     graph_pos.plot(t, x)
     graph_vel.plot(t, v)
     graph_acc.plot(t, a)
     print(f"Reached goald at t = {round(t, 2)} secs")
-
-
-# Workshop Class notes
-class Person:
-    def __init__(self, name, alter):
-        self.name = name
-        self.alter = alter
-
-"""
-class Person {
-public:
-    Person(std::string name, int alter) {
-        this->name = name;
-        this->alter = alter;
-    }
-
-private:
-    std::string name;
-    int alter;
-};
-"""
+    print("v = ", v ," m")
